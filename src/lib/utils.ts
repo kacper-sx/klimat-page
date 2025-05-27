0eb4cd72ca8a0ad5a802abd419465934190cb4f8
kacper-sx <kacper.saweczko@o2.pl>
2025-05-27 12:52:44 +0200

    feat: add certificates section

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,6 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-
-
-export const qsAll = <T extends string>(selector: T) => {
-  const els = document.querySelectorAll(selector)
-  return Array.from(els).filter(el => el instanceof HTMLElement)
-} 
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
